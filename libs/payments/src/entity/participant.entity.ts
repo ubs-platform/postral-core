520427001d6b8de1851897b65a87a3e17750798b
Hüseyin Can Gündüz <hcangunduz@gmail.com>
2025-04-07 11:26:37 +0300

    participant

---
+++ libs/payments/src/entity/participant.entity.ts
@@ -0,0 +1,23 @@
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { Payment } from './payment.entity';
+
+@Entity()
+export class Participant {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    personalityType: 'INDUVIDIAL' | 'COMMERCIAL';
+
+    fullName: string;
+
+    /***
+     * Türkiye, ABD, vs. vatandaşı olduğu
+     * */
+    identityCountry: string;
+    /**
+     * Türkiye:
+     * TCKN ya da VKN
+     * Diğer ülkeler: Vergi numarası
+     */
+    identityNumber: string;
+}
