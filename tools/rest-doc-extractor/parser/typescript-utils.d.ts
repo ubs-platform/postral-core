ac24ba00d05363a52a55c379e958b1ecf6be6034
Hüseyin Can Gündüz <hcangunduz@gmail.com>
2025-09-22 15:14:42 +0300

    service name

--- tools/rest-doc-extractor/parser/typescript-utils.d.ts
+++ tools/rest-doc-extractor/parser/typescript-utils.d.ts
@@ -2,7 +2,7 @@ import { ParameterDeclaration, Type } from "ts-morph";
 import { RestPrimitiveTypeInfo } from "./api.data.js";
 import { ts } from "ts-morph";
 export declare class TypescriptNestUtils {
-    static extractFromPromise(t: Type): any;
+    static extractFromPromise(t: Type): Type<ts.Type>;
     static findImportSource(type: Type<ts.Type>): string;
     static extractRestMethodPrimitiveParameterInfo(param: ParameterDeclaration): RestPrimitiveTypeInfo[];
     private static propertiesFromType;
