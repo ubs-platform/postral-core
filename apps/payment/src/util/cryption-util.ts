6cf670f551b557b8736bd2f3b9812edf5cc39cd1
Hüseyin Can Gündüz <hcangunduz@gmail.com>
2026-04-18 19:26:45 +0300

    dev

--- apps/payment/src/util/cryption-util.ts
+++ apps/payment/src/util/cryption-util.ts
@@ -4,6 +4,10 @@ import { Injectable } from "@nestjs/common";
 export class CryptionUtil {
 
     encryptWithConfig(text: string | null | undefined, onError: "THROW" | "USE_DEFAULT" = "THROW"): string {
+        if (process.env.POSTRAL_SENSITIVE_DATA_ENCRYPTION_ENABLED !== "true") {
+            console.warn('Encryption is disabled. Returning original text.');
+            return text || ""; // Return original text if encryption is disabled, or empty string if input is null or undefined
+        }
         if (!text) {
             return text || ""; // Return empty string if input is null or undefined
         }
@@ -25,10 +29,16 @@ export class CryptionUtil {
     }
 
     decryptWithConfig(encryptedText: string | null | undefined, onError: "THROW" | "USE_DEFAULT" = "THROW"): string {
+        if (process.env.POSTRAL_SENSITIVE_DATA_ENCRYPTION_ENABLED !== "true" && process.env.POSTRAL_SENSITIVE_DATA_ENCRYPTION_ENABLED !== "false-decrypt") {
+            console.warn('Decryption is disabled. Returning original encrypted text.');
+            return encryptedText || ""; // Return original encrypted text if decryption is disabled, or empty string if input is null or undefined
+        }
         if (!encryptedText) {
             return encryptedText || ""; // Return empty string if input is null or undefined
         }
-        
+        // false-decrypt durumunda, şifrelenmiş veriyi çözümleyebiliriz ancak şifreleme işlemi devre dışı bırakılmıştır. Bu durumda, şifrelenmiş veriyi çözümleyebiliriz ancak yeni veriler şifrelenmeyecektir.
+        // Bu durumda development ortamlarının uyumluluğu için her veri bir kere kaydedilmesi gerekiyor...
+
         try {
             
             const key = Buffer.from(process.env.POSTRAL_SENSITIVE_DATA_ENCRYPTION_KEY!, 'utf-8');
