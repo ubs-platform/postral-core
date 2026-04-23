cd61e74c7a1cc7ee76eca45896dbb1d1946ea313
Hüseyin Can Gündüz <hcangunduz@gmail.com>
2026-04-23 13:39:58 +0300

    dev

--- apps/payment/src/entity/admin-settings.entity.ts
+++ apps/payment/src/entity/admin-settings.entity.ts
@@ -1,6 +1,8 @@
 import { Column, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn, Unique } from 'typeorm';
 import { Payment } from './payment.entity';
 import { Account } from './account.entity';
+import { ReportQuery } from './report-query.entity';
+import { ItemTaxEntity } from './item-tax.entity';
 
 @Entity()
 export class AdminSettings {
@@ -17,10 +19,32 @@ export class AdminSettings {
 
     /**
      * Eğer true ise net gelir üzerinden komisyon hesaplanır, false ise brüt gelir üzerinden hesaplanır. 
-     * TODO: Özellik implement edilecek
      */
     @Column({ type: "boolean", default: false })
     comissionsCalculatedFromNet: boolean = false;
+
+    @Column({ type: "uuid", nullable: true })
+    reportQueryId?: string;
+
+    // Komisyonların hangi raporlama sorgusuna göre hesaplanacağını belirler.
+    //  Admin tarafından seçilecek. 
+    // Seçilen raporlama sorgusu, rapor digestion tarafından kullanılacak ve rapor digestion, 
+    // komisyonları hesaplamak için bu sorguyu kullanacak.
+    // Raporlama sorgusu DAILY (Günlük) olmalıdır. Yoksa gün gün hesaplamalar yapılamaz ve daha kod bakımı daha meşakatli olur.
+    // Satıcıya komisyonu faturalandırma ve satıcının kazancını platforma göre hesaplama işlemi, 
+    // rapor digestion tarafından yapılacak ve rapor digestion, komisyonları hesaplamak için bu 
+    // sorguyu kullanacak. 
+    @ManyToOne(() => ReportQuery, { nullable: true, eager: false })
+    @JoinColumn({ name: 'reportQueryId' })
+    reportQuery?: ReportQuery;
+
+    @Column({ type: "uuid", nullable: true })
+    comissionItemTaxId?: string;
+
+    // Komisyonlarda kullanılacak vergi oranı. Admin tarafından seçilecek. Seçilen vergi oranı, ürünlerde kullanılan tax entitysi içerisinden seçilecek.
+    @ManyToOne(() => ItemTaxEntity, { nullable: true, eager: false })
+    @JoinColumn({ name: 'comissionItemTaxId' })
+    comissionItemTax?: ItemTaxEntity;
 
 
     @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
