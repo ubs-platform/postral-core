e6f2e04ba4b0d6db54a9aa9dafd8afb141395463
Hüseyin Can Gündüz <hcangunduz@gmail.com>
2026-07-20 19:44:15 +0300

    testo app exclusion

--- tools/release-all-apps.sh
+++ tools/release-all-apps.sh
@@ -1,4 +1,12 @@
+# !/bin/bash
+#skip "testo" app for now, because it is not ready for release yet
+
 APPS=$(ls apps)
 for APP in $APPS; do
+    if [ "$APP" == "testo" ]; then
+        echo "Skipping $APP"
+        continue
+    fi
+    echo "Releasing $APP"
     $(dirname ${BASH_SOURCE[0]})/release-app.sh $APP
 done
