4ae4f1d944b03956b497b66b4b6c9514c1a9a702
Hüseyin Can Gündüz <hcangunduz@gmail.com>
2026-08-13 20:46:49 +0300

    note added for v2

--- apps/payment/src/main.ts
+++ apps/payment/src/main.ts
@@ -27,6 +27,42 @@ async function bootstrap() {
     if (process.argv.includes('--v2-payment-snapshot-migration')) {
         const a = app.get(V2MigrationUtil);
         await a.doMigration();
+    } else {
+        console.warn(`
+            -------------------DİKKAT--------------------
+            Versiyon 2'de Payment yapısında büyük değişiklikler yapıldı. Bu nedenle versiyon 1'deki Payment verilerini versiyon 2'ye taşımak için migration işlemi yapılması gerekmektedir.
+            Bu işlemi yaptıysanız dikkate almayınız. Eğer yapmadıysanız, Migration işlemi için uygulama başlatılırken --v2-payment-snapshot-migration parametresi ile başlatılması gerekmektedir.
+            Örnek: npm run start -- --v2-payment-snapshot-migration
+            Docker compose dosyasında da bu bu parametreyi ekledikten sonra yeniden başlatabilirsiniz.
+
+            \`\`\`
+            services:
+                postral-core-api:
+                    ## Diğer parametrelere dokunmayın
+                    command: node /app/dist/main.js --v2-payment-snapshot-migration
+            \`\`\`
+
+            Bu parametre, bir sonraki minör sürümlerde kaldırılacaktır. Bu nedenle, versiyon 1'den versiyon 2'ye geçiş yapıldıktan sonra bu parametreyi kullanmayı bırakmanız önerilir.
+            ---------------------------------------------
+            
+            ----------ATTENTION----------
+            In version 2, major changes were made to the Payment structure. Therefore, it is necessary to perform a migration process to transfer Payment data from version 1 to version 2.
+            If you have done this, please disregard. If you haven't, you need to start the application with the --v2-payment-snapshot-migration parameter for the migration process.
+            To perform the migration process, the application must be started with the --v2-payment-snapshot-migration parameter.
+            Example: npm run start -- --v2-payment-snapshot-migration
+            You can also add this parameter to the Docker compose file and restart it after adding it.
+            \`\`\`
+            services:
+                postral-core-api:
+                    ## Do not edit other parameters
+                    command: node /app/dist/main.js --v2-payment-snapshot-migration
+            \`\`\`
+           
+            This parameter will be removed in the next minor releases. Therefore, it is recommended to stop using this parameter after migrating from version 1 to version 2.
+            ---------------------------------------------
+            
+            
+            `);
     }
     await app.listen(port, '0.0.0.0');
     Logger.log(
